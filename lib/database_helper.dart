93872e68f8491f3ce4460004236a291e98b71f2a
Oscar Silva <osgisilo@gmail.com>
2024-01-17 15:02:17 -0700

    funciones de base de datos agregfradas..

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -3,19 +3,18 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:telsolreclutamiento/modelos/jefDeReclutamiento.dart';
 import 'package:telsolreclutamiento/modelos/reclutador.dart';
+import 'package:telsolreclutamiento/modelos/procesoDeReclutamiento.dart';
+import 'package:telsolreclutamiento/modelos/prospecto.dart';
 
 class database_helper {
   final datbaseName = "telsol.db";
   String JefeRecluTable = "CREATE TABLE JEFEDERECLU (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, password TEXT)";
-
   String JefeDeReclu = "INSERT INTO JEFEDERECLU (username, password) VALUES ('Vladimir Jimenez','Telsol01')";
   String ReclutadorTable = "CREATE TABLE RECLUTADOR (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, password TEXT, habilitado INTEGER)";
   String pruebaReclutador = "INSERT INTO RECLUTADOR (username, password, habilitado) VALUES ('Judith Garcia','Telsol01',0)";
-
   String createProspectoTable = "CREATE TABLE PROSPECTO (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, "
       "primerApellido TEXT, segundoApellido TEXT, direccion TEXT, telefono TEXT, escolaridad TEXT, edad INTEGER, calquizz INTEGER,"
       " calexamTec INTEGER, calexamAud INTEGER, campana TEXT, estatus TEXT, motivo TEXT)";
-
   String createProcesoTable = "CREATE TABLE PROCESO (id INTEGER PRIMARY KEY "
       "AUTOINCREMENT, nombreReclutador TEXT, idReclutante INTEGER, idProspecto INTEGER, pts TEXT)";
 
@@ -56,6 +55,98 @@ class database_helper {
     }
   }
 
+  // METODOS DE CREAR
 
+  Future<int> crearReclutador(Reclutador reclu) async {
+    final Database db = await initDB();
+    return db.insert('RECLUTADOR', reclu.toMap());
+  }
+
+  Future<int> crearProspecto(Prospecto pros) async {
+    final Database db = await initDB();
+    return db.insert('Prospecto', pros.toMap());
+  }
+
+  Future<int> crearProceso(ProccesoDeContratacion pros) async{
+    final Database db = await initDB();
+    return db.insert('PROCESO', pros.toMap());
+  }
+
+  //METODOS DE BUSCAR
+
+  Future<List<Reclutador>> getReclutadores() async {
+    final Database db = await initDB();
+    List<Map<String, Object?>> result = await db.query('RECLUTADOR');
+    return result.map((e) => Reclutador.fromMap(e)).toList();
+  }
+  Future<List<Reclutador>> buscarReclutador(String keyword) async{
+    final Database db = await initDB();
+    List<Map<String, Object?>> resultadobusqeda = await db.
+    rawQuery("SELECT * FROM RECLUTADOR WHERE username LIKE ?", ["%$keyword%"]);
+    return resultadobusqeda.map((e) => Reclutador.fromMap(e)).toList();
+  }
+
+  Future<List<Reclutador>> buscarReclutadorPorId(int id) async {
+    final Database db = await initDB();
+    List<Map<String, Object?>> resultado = (await
+    db.rawQuery("SELECT * FROM RECLUTADOR WHERE id = ?", ["%$id%"]));
+    return resultado.map((e) => Reclutador.fromMap(e)).toList();
+  }
+
+  Future<List<Prospecto>> getProspectos() async {
+    final Database db = await initDB();
+    List<Map<String, Object?>> result = await db.query('Prospecto');
+    return result.map((e) => Prospecto.fromMap(e)).toList();
+  }
+
+  Future<List<Prospecto>> searchProspecto(String keyword) async {
+    final Database db = await initDB();
+    List<Map<String, Object?>> searchResult = await db.rawQuery(
+        "SELECT * FROM PROSPECTO WHERE nombre || ' ' || primerApellido || ' ' || segundoApellido LIKE ? ", ["%$keyword%"]);
+    return searchResult.map((e) => Prospecto.fromMap(e)).toList();
+  }
+
+  Future<List<ProccesoDeContratacion>> buscarProcesoPorFecha(String desde, hasta) async {
+    final Database db = await initDB();
+    List<Map<String, Object?>> resultado = await db.rawQuery("SELECT * FROM PROCESO WHERE pts > ? and pts < ?", [desde,hasta]);
+    return resultado.map((e) => ProccesoDeContratacion.fromMap(e)).toList();
+  }
+
+  //METODOS DE BORRAR
+
+  Future<int> borrarReclutador(int id) async {
+    final Database db = await initDB();
+    return db.delete('RECLUTADOR', where: 'id=?', whereArgs: [id]);
+  }
+
+  Future<int> borrarProspecto(int id) async {
+    final Database db = await initDB();
+    return db.delete('Prospecto', where: 'id = ?', whereArgs: [id]);
+  }
+
+  //METODOS DE ACTUALIZAR
+
+  Future<int> editarReclutador(username, password, habilitado, id) async{
+    final Database db = await initDB();
+    return db.rawUpdate(
+        'UPDATE RECLUTADOR SET username = ?, password = ?, habilitado = ? WHERE id = ?',
+        [username,password,habilitado, id]);
+  }
+
+  Future<int> editarProspecto(nombre, PA, SA, D, T, ESC, EDAD, CAMP, EST, MOT,ID) async {
+    final Database db = await initDB();
+    return db.rawUpdate(
+        'UPDATE PROSPECTO SET nombre = ?, primerApellido = ?, '
+            'segundoApellido = ?, direccion = ?, telefono = ?, '
+            'escolaridad = ?, edad = ?, campana = ?, estatus =?, motivo = ?'
+            'WHERE id = ?'
+        ,[nombre, PA,SA,D,T,ESC, EDAD, CAMP, EST, MOT,ID]
+    );
+  }
+
+  Future<int> editarCalificacionProspecto(calquizz, calexamTec, calexamAud, id) async {
+    final Database db = await initDB();
+    return db.rawUpdate('UPDATE PROSPECTO SET calquizz = ?, calexamTec = ?, calexamAud = ? WHERE id = ?',[calquizz, calexamTec, calexamAud, id]);
+  }
 
 }
